9ed637c2d04e75893ffa73d7254dcb6abdca0374
RedGalaxySW <onankman@gmail.com>
2020-06-26 11:38:49 +0200

    Push IDEA code style and inspection profiles to maintain code consistency...

--- src/main/java/midnight/client/ClientEventHandler.java
+++ src/main/java/midnight/client/ClientEventHandler.java
@@ -10,10 +10,12 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod.EventBusSubscriber(Dist.CLIENT)
-public class ClientEventHandler
-{
-    private static final Logger LOGGER = LogManager.getLogger("The Midnight");
+@Mod.EventBusSubscriber( Dist.CLIENT )
+public final class ClientEventHandler {
+    private static final Logger LOGGER = LogManager.getLogger( "The Midnight" );
+
+    private ClientEventHandler() {
+    }
 
     /*
      * This method is fired whenever LoggedInEvent is called.
@@ -21,16 +23,14 @@ public class ClientEventHandler
      * The message is also sent to the console at WARN level.
      */
     @SubscribeEvent
-    public static void loggedInEvent(ClientPlayerNetworkEvent.LoggedInEvent event)
-    {
+    public static void loggedInEvent( ClientPlayerNetworkEvent.LoggedInEvent event ) {
         ClientPlayerEntity player = event.getPlayer();
 
-        if (player != null)
-        {
-            LOGGER.warn("The Midnight for Minecraft 1.16.1 is still under active development. " +
-                    "Many features will be missing and/or completely unusable. You have been warned!");
-            event.getPlayer().sendMessage(new TranslationTextComponent("The Midnight for Minecraft 1.16.1 is still under active development. " +
-                    "Many features will be missing and/or completely unusable. You have been warned!").func_240699_a_(TextFormatting.RED), event.getPlayer().getUniqueID());
+        if( player != null ) {
+            LOGGER.warn( "The Midnight for Minecraft 1.16.1 is still under active development. " +
+                             "Many features will be missing and/or completely unusable. You have been warned!" );
+            event.getPlayer().sendMessage( new TranslationTextComponent( "The Midnight for Minecraft 1.16.1 is still under active development. " +
+                                                                             "Many features will be missing and/or completely unusable. You have been warned!" ).func_240699_a_( TextFormatting.RED ), event.getPlayer().getUniqueID() );
         }
     }
 }
