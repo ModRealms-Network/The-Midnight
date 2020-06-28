d9c72fdaa8abe96f4e10614286318b67906490dd
RedGalaxySW <onankman@gmail.com>
2020-06-28 23:29:30 +0200

    Code consistency: Made BuildTestHandler final - All event handler classes are final

--- MidnightMod/src/main/java/midnight/common/handler/BuildTestHandler.java
+++ MidnightMod/src/main/java/midnight/common/handler/BuildTestHandler.java
@@ -1,24 +1,20 @@
 package midnight.common.handler;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.server.MinecraftServer;
 import midnight.MidnightInfo;
-import midnight.MidnightMod;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 @Mod.EventBusSubscriber(Dist.DEDICATED_SERVER)
-public class BuildTestHandler {
+public final class BuildTestHandler {
     private static final Logger LOGGER = LogManager.getLogger("MidnightMod");
 
     private BuildTestHandler() {
