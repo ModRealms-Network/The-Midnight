71c344c2f9251e51860777b9b01320540182add9
Jonathan Colmenares <jonathing.projects@gmail.com>
2020-07-02 15:27:34 -0400

    Deprecating the batch files

--- Windows Batch Files/eclipse.bat
+++ Windows Batch Files/eclipse.bat
@@ -1,5 +1,8 @@
 @echo off
 title Generating eclipse workspace...
-cd..
+echo Please note that the Windows batch files may be removed in a later commit.
+echo Instructions on how to use Gradle with this project are kept in GRADLE.md.
+echo.
+cd ..
 call gradlew.bat eclipse
 pause
--- Windows Batch Files/genIntellijRuns.bat
+++ Windows Batch Files/genIntellijRuns.bat
@@ -1,5 +1,8 @@
 @echo off
 title Making IntelliJ IDEA run files..
-cd..
+echo Please note that the Windows batch files may be removed in a later commit.
+echo Instructions on how to use Gradle with this project are kept in GRADLE.md.
+echo.
+cd ..
 call gradlew.bat genIntellijRuns
 pause
